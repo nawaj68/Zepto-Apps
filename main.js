b94d618339a0a0d0cc27d72e7e8ff865452c5d9f
nawaj68 <nawajshah68@gmail.com>
2024-05-14 22:49:07 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ <script>
+      function toggleMenu() {
+        var menu = document.getElementById("user-menu");
+        if (menu.style.display === "none") {
+          menu.style.display = "block";
+        } else {
+          menu.style.display = "none";
+        }
+      }
+    </script>
